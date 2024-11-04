aa727912de2bc9c6e2653f46a86aa75c516ae996
nameerakhter <akhtarnameer@gmail.com>
2024-11-04 18:55:21 +0530

    feat: setup github authentication using nextauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,31 @@
 "use client";
-import { useState } from "react";
+
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-    setFile(file);
-    if (fileUrl) {
-      URL.revokeObjectURL(fileUrl);
-    }
-    if (file) {
-      const url = URL.createObjectURL(file);
-      setFileUrl(url);
-    } else {
-      setFileUrl(undefined);
-    }
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <button
+        onClick={() => signIn("github")}
+        className="bg-white text-black p-4 font-bold  rounded-xl"
+      >
+        SignIn
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={() => signOut()}
+        className="bg-white text-black p-4 font-bold  rounded-xl"
+      >
+        SignOut
+      </button>
+    );
   }
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    console.log(file);
-  }
-
   return (
-    <div className="flex justify-center items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col items-center gap-3  p-4  my-auto">
-          <input
-            type="file"
-            placeholder="upload file"
-            className="p-4 rounded-xl border-white border-2"
-            accept="image/*,video/*" // Allows only image and video files
-            onChange={handleChange}
-          />
-          {fileUrl && file && (
-            <div className="mt-4">
-              {file.type.startsWith("image/") ? (
-                <img width="300" src={fileUrl} alt="Selected file" />
-              ) : file.type.startsWith("video/") ? (
-                <video src={fileUrl} controls width="300" muted />
-              ) : null}
-            </div>
-          )}
-          <button
-            type="submit"
-            className="bg-white text-black rounded-xl px-4 border-2 border-white"
-          >
-            Upload
-          </button>
-        </div>
-      </form>
-    </div>
+    <>
+      <h1>User SignIn/LoginPage</h1>;
+    </>
   );
 }
---
+++ app/upload.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+export function Upload() {
+  const [file, setFile] = useState<File | undefined>(undefined);
+  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    setFile(file);
+    if (fileUrl) {
+      URL.revokeObjectURL(fileUrl);
+    }
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setFileUrl(url);
+    } else {
+      setFileUrl(undefined);
+    }
+  }
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log(file);
+  }
+
+  return (
+    <div className="flex justify-center items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col items-center gap-3  p-4  my-auto">
+          <input
+            type="file"
+            placeholder="upload file"
+            className="p-4 rounded-xl border-white border-2"
+            accept="image/*,video/*" // Allows only image and video files
+            onChange={handleChange}
+          />
+          {fileUrl && file && (
+            <div className="mt-4">
+              {file.type.startsWith("image/") ? (
+                <img width="300" src={fileUrl} alt="Selected file" />
+              ) : file.type.startsWith("video/") ? (
+                <video src={fileUrl} controls width="300" muted />
+              ) : null}
+            </div>
+          )}
+          <button
+            type="submit"
+            className="bg-white text-black rounded-xl px-4 border-2 border-white"
+          >
+            Upload
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
