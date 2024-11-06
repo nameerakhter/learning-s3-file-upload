a1619b53b61a9fb210f68b0576659d20e14401be
nameerakhter <akhtarnameer@gmail.com>
2024-11-06 17:18:09 +0530

    feat: upload data to s3 & error handling

--- app/create/action.ts
+++ app/create/action.ts
@@ -1,5 +1,30 @@
 "use server";
 
-export async function getSignedUrl() {
-  return { success: { url: "" } };
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+const s3 = new S3Client({
+  region: process.env.AWS_BUCKET_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
+export async function getUrl() {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return { failure: "Not Authenticated" };
+  }
+
+  const putObjectCommand = new PutObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME!,
+    Key: "test-file",
+  });
+  const signedUrl = await getSignedUrl(s3, putObjectCommand, {
+    expiresIn: 60,
+  });
+  return { success: { url: signedUrl } };
 }
--- app/upload.tsx
+++ app/upload.tsx
@@ -1,55 +1,70 @@
-import { useState } from "react";
-
-export function Upload() {
-  const [file, setFile] = useState<File | undefined>(undefined);
-  const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
-  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-    setFile(file);
-    if (fileUrl) {
-      URL.revokeObjectURL(fileUrl);
-    }
-    if (file) {
-      const url = URL.createObjectURL(file);
-      setFileUrl(url);
-    } else {
-      setFileUrl(undefined);
-    }
-  }
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    console.log(file);
-  }
-
-  return (
-    <div className="flex justify-center items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col items-center gap-3  p-4  my-auto">
-          <input
-            type="file"
-            placeholder="upload file"
-            className="p-4 rounded-xl border-white border-2"
-            accept="image/*,video/*" // Allows only image and video files
-            onChange={handleChange}
-          />
-          {fileUrl && file && (
-            <div className="mt-4">
-              {file.type.startsWith("image/") ? (
-                <img width="300" src={fileUrl} alt="Selected file" />
-              ) : file.type.startsWith("video/") ? (
-                <video src={fileUrl} controls width="300" muted />
-              ) : null}
-            </div>
-          )}
-          <button
-            type="submit"
-            className="bg-white text-black rounded-xl px-4 border-2 border-white"
-          >
-            Upload
-          </button>
-        </div>
-      </form>
-    </div>
-  );
-}
+// import { useState } from "react";
+// import { getUrl } from "./create/action";
+// import { NextResponse } from "next/server";
+//
+// export function Upload() {
+//   const [file, setFile] = useState<File | undefined>(undefined);
+//   const [fileUrl, setFileUrl] = useState<string | undefined>(undefined);
+//   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+//     const file = e.target.files?.[0];
+//     setFile(file);
+//     if (fileUrl) {
+//       URL.revokeObjectURL(fileUrl);
+//     }
+//     if (file) {
+//       const url = URL.createObjectURL(file);
+//       setFileUrl(url);
+//     } else {
+//       setFileUrl(undefined);
+//     }
+//   }
+//   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+//     e.preventDefault();
+//     const getSignedUrlResult = await getUrl();
+//     console.log(getSignedUrlResult);
+//     if (getSignedUrlResult.failure !== undefined) {
+//       return NextResponse.json(
+//         {
+//           message: "failed",
+//         },
+//         { status: 400 },
+//       );
+//     }
+//
+//     const url = getSignedUrlResult.success.url;
+//     console.log({ url });
+//
+//     console.log(file);
+//   }
+//
+//   return (
+//     <div className="flex justify-center items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+//       <form onSubmit={handleSubmit}>
+//         <div className="flex flex-col items-center gap-3  p-4  my-auto">
+//           <input
+//             type="file"
+//             placeholder="upload file"
+//             className="p-4 rounded-xl border-white border-2"
+//             accept="image/*,video/*" // Allows only image and video files
+//             onChange={handleChange}
+//           />
+//           {fileUrl && file && (
+//             <div className="mt-4">
+//               {file.type.startsWith("image/") ? (
+//                 <img width="300" src={fileUrl} alt="Selected file" />
+//               ) : file.type.startsWith("video/") ? (
+//                 <video src={fileUrl} controls width="300" muted />
+//               ) : null}
+//             </div>
+//           )}
+//           <button
+//             type="submit"
+//             className="bg-white text-black rounded-xl px-4 border-2 border-white"
+//           >
+//             Upload
+//           </button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
